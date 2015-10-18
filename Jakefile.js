db498fd3a63af480ba782f5b215713bfc1116bd7
Jacob Foshee <jacobf@gmail.com>
2015-10-18 19:07:01 +0000

    Using a file dependency to build geojson conditionally

--- Jakefile.js
+++ Jakefile.js
@@ -1,10 +1,16 @@
 var makeGis = require("./makeGis");
 
+// jakejs functions:
 /* global desc */
 /* global task */
+/* global file */
 
 desc("The default task builds everything...");
-task("default", function (params) {
-  console.log("Starting build...");
-  makeGis.make2DSurveyGeoJson();
+task("default", ["2DNavigationLinesA-E.geojson"], function (params) {
+    console.log("Starting build...");
+});
+
+desc("Update the 2D survey geojson");
+file("2DNavigationLinesA-E.geojson", ["source/2D_Seismic/2DNavigationLinesA-E.txt"], function() {
+    makeGis.make2DSurveyGeoJson();
 });
